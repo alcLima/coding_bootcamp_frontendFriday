5ebdfafb0e30399b5ec6281c5ed738d51f6f7d7e
Ana Luiza <alclim02@gmail.com>
2024-06-01 17:03:27 -0400

    build: install libs and background

---
+++ src/Components/Main.jsx
@@ -0,0 +1,11 @@
+const Main = () => {
+  return (
+    <main className="w-full h-full grid-cols-2">
+      <div className=""></div>
+      <img src="public/images/pattern-curve.svg" alt="bckg-curve" />
+      <img src="public/images/pattern-bg.svg" alt="bckg-shape" />
+    </main>
+  );
+};
+
+export default Main;
